555eef11e23b5b56c675c88b95b2b2d6bf9adea8
Nik Malhotra <nmal@me.com>
2019-02-19 03:38:53 +0000

    initial setup

---
+++ Battleship.js
@@ -0,0 +1,46 @@
+const genBoard = () => {
+  return [
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
+  ];
+
+};
+
+const leaderBoard = {};
+
+
+const makeHit = (x, y) => {
+  // run this every time a move needs to be made
+  // return hit vs miss
+  // call a different function to see if the ship got sunk
+  return;
+};
+
+const didShipSink = board => {
+  //  did the ship sink yet? this is player/computer agnostic.
+  return;
+};
+
+const newGame = args => {
+  // start new game. clear board, start the whole dam thing again.
+  return;
+}
+
+const makeTurn = args => {
+  return;
+}
+
+const dumbAI = args => {
+  // this AI will shoot at random. take two random numbers and use those two numbers as coordinates
+}
+
+
+
